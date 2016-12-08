e70c02f880737a4f960c1cb7c4550457459cd061
Simon Christmann <simon@christmann.email>
2016-12-08 23:41:48 +0100

    Clean up LED code

--- FastLED.ino
+++ FastLED.ino
@@ -7,6 +7,9 @@ Thread led_show = Thread();
 Thread led_animation = Thread();
 
 uint8_t baseHue = 0;
+uint8_t hue = 0;
+uint8_t saturation = 0;
+uint8_t brightness = 255;
 
 void setup_fastled() {
   // this resets all the neopixels to an off state
@@ -44,66 +47,94 @@ void led_animation_func() {
 
 void led_input(String topic, String inputString) {
   if (topic == LED_SOLID_TOPIC) {
-    led_solid(inputString);
-  }
-  if (topic == LED_ANIMATION_TOPIC) {
-    led_animation_msg(inputString);
+    if (inputString == "ON") {
+      led_on();
+    } else if (inputString == "OFF") {
+      led_off();
+    } else {
+      led_solid(inputString);
+    }
+  } else if (topic == LED_ANIMATION_TOPIC) {
+    if (inputString == "OFF") {
+      led_off();
+    } else {
+      led_animation_msg(inputString);
+    }
   }
 }
 
 void led_solid(String inputString) {
-  led_animation.enabled = false;
-  Serial.println("input");
+  Serial.println("led_solid input");
+  
   String results[10];
   uint8_t count = explode(results, inputString, ',');
-    for (uint8_t i = 0; i < count; i++) {
+  for (uint8_t i = 0; i < count; i++) {
     Serial.print("i = ");Serial.println(i);
     Serial.print("result = ");Serial.println(results[i]);
   }
-  
-  // hue 0-360
-  float raw_hue = (results[0]).toFloat();
-  uint8_t hue;
-  if (raw_hue < 1.0) { 
-    hue = 0;
-  } else {
-    hue = raw_hue / 360.0 * 255.0;
-  }
-  //Serial.print("hue = ");Serial.println(hue);
-  // saturation 0-100
-  uint8_t saturation = (results[1]).toFloat() / 100.0 * 255.0;
-  //Serial.print("saturation = ");Serial.println(saturation);
-  // brightness 0-100
-  float raw_brightness = (results[2]).toFloat();
-  uint8_t brightness;
-  if (raw_brightness < 1.0) { 
-    brightness = 0;
-  } else {
-    brightness = raw_brightness / 100.0 * 255.0;
-  }
-  //Serial.print("brightness = ");Serial.println(brightness);
-  Serial.println("conversion");
-  fill_solid( leds, NUM_LEDS, CHSV( hue, saturation, brightness));
 
+  if ( count != 3 ) return;
+
+  // Hue 0-360
+  uint8_t hue_tmp;
+  float hue_raw = (results[0]).toFloat();
+  hue_tmp = hue_raw / 360.0 * 255.0;
+
+  // Saturation 0-100
+  uint8_t sat_tmp;
+  float sat_raw = (results[1]).toFloat();
+  sat_tmp = sat_raw / 100.0 * 255.0;
+
+  // Brightness 0-100
+  uint8_t bri_tmp;
+  float bri_raw = (results[2]).toFloat();
+  if (bri_raw < 1.0) { 
+    bri_tmp = 0;
+  } else {
+    bri_tmp = bri_raw / 100.0 * 255.0;
+  }
+
+  Serial.println("conversion done");
+
+  //TODO: check values
+  hue = hue_tmp;
+  saturation = sat_tmp;
+  brightness = bri_tmp;
+
+  // Output
+  led_fillSolidColor();
+  postNewState();
+}
+
+void postNewState() {
   String state = String(hue / 255.0 * 360.0);
   state += ",";
   state += String(saturation / 255.0 * 100.0);
   state += ",";
   state += String(brightness / 255.0 * 100.0);
-  char msg[50];
-  state.toCharArray(msg, 50);
-
-  mqttClient.publish(LED_STATE_TOPIC, msg);
+  
+  mqtt_publish(LED_STATE_TOPIC, state);
 }
+
+void led_fillSolidColor() {
+  led_animation.enabled = false;
+  fill_solid( leds, NUM_LEDS, CHSV( hue, saturation, brightness));
+}
+void led_on() {
+  brightness = 255;
+  led_fillSolidColor();
+  postNewState();
+}
+void led_off() {
+  brightness = 0;
+  led_fillSolidColor();
+  postNewState();
+}
+
 void led_animation_msg(String inputString) {
   if (inputString == "sinelon") {
     led_animation.enabled = true;
     mqttClient.publish(LED_STATE_TOPIC, "sinelon");
-  }
-  if (inputString == "OFF") {
-    led_animation.enabled = false;
-    fill_solid( leds, NUM_LEDS, CRGB::Black);
-    mqttClient.publish(LED_STATE_TOPIC, "OFF");
   }
 }
 
