cd0aa4b1888d67703aff0f9eba8df64388dfe866
Simon Christmann <simon@christmann.email>
2016-12-09 00:27:00 +0100

    Remove double definition; configure for NodeMCU

--- default_config.h
+++ default_config.h
@@ -3,14 +3,14 @@ char* mqtt_id       = "WifiSensorBoard-0001";
 
 char* mqtt_uptime_topic = "/debug/fastled/uptime";
 
-#define DHTPIN 5
+#define DHTPIN D2
 #define DHT_SENSOR_TYPE DHT_TYPE_22
 #define DHT_INTERVAL 10000
 #define DHT_TEMPERATURE_TOPIC "/temperature/dht"
 #define DHT_HUMIDITY_TOPIC "/humidity/dht"
 
-#define ONE_WIRE_BUS 2
-#define PULLUP_PIN 14
+#define ONE_WIRE_BUS D7
+#define PULLUP_PIN D0
 #define TEMPERATURE_PRECISION 9
 #define MAX_DEVICES 5
 #define DALLAS_BASETOPIC "/temperature/"
@@ -24,15 +24,3 @@ char* mqtt_uptime_topic = "/debug/fastled/uptime";
 #define LED_SOLID_TOPIC "/debug/led/input"
 #define LED_ANIMATION_TOPIC "/debug/led/animation"
 
-// Definitions for Node MCU Board
-static const uint8_t D0   = 16;
-static const uint8_t D1   = 5;
-static const uint8_t D2   = 4;
-static const uint8_t D3   = 0;
-static const uint8_t D4   = 2;
-static const uint8_t D5   = 14;
-static const uint8_t D6   = 12;
-static const uint8_t D7   = 13;
-static const uint8_t D8   = 15;
-static const uint8_t D9   = 3;
-static const uint8_t D10  = 1;
