bfab638317a3898dd1cb6ae52a033382ec53ddcd
Simon Christmann <simon@christmann.email>
2017-09-15 19:22:23 +0200

    Implement new libraries in main file

--- WifiSensorBoard.ino
+++ WifiSensorBoard.ino
@@ -2,40 +2,85 @@
 #include "default_wifi.h"
 #include "handy_functions.h"
 
-// --------------------------------------------------------
+#include <GenericLog.h>         // https://github.com/dersimn/ArduinoUnifiedLog
+#include <NamedLog.h>
+#include <LogHandler.h>
+#include <LogSerialModule.h>
 
-#include <ESP8266WiFi.h>        // Wifi Library
-#include <PubSubClient.h>       // MQTT Library
-#include <Thread.h>
-#include <ThreadRunOnce.h>
+#include <Thread.h>             // https://github.com/ivanseidel/ArduinoThread
 #include <ThreadController.h>
+#include <ThreadRunOnce.h>      // https://github.com/dersimn/ArduinoThreadRunOnce
 
-#include <DallasTemperature.h>  // Throws compiler error if not specified here
+#include <WiFiManager.h>        // https://github.com/tzapu/WiFiManager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+
+#include <PubSubClient.h>       // https://github.com/knolleary/pubsubclient
+
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+
+#include <FastLED.h>            // https://github.com/FastLED/FastLED
+#include <NeoPixelBus.h>        // https://github.com/Makuna/NeoPixelBus
 
 // --------------------------------------------------------
+
+#include <DallasTemperature.h>  // Throws compiler error if not specified in main file
+
+// --------------------------------------------------------
+
+LogHandler logHandler;
+LogSerialModule serialModule(115200);
+
+GenericLog Log    (logHandler);
+NamedLog   LogWiFi(logHandler, "WiFi");
+NamedLog   LogMqtt(logHandler, "MQTT");
 
 ThreadController threadControl = ThreadController();
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
+String BOARD_ID = String("WifiSensor")+ESP.getChipId();
+char   BOARD_ID_CHAR[50];
+
 // --------------------------------------------------------
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial); // wait for serial port initialization
+  BOARD_ID.toCharArray(BOARD_ID_CHAR, 50);
 
-  setup_wifi();
-  setup_mqtt();
-  setup_uptime();
-  setup_sensor_dht();
-  setup_dallas();
+  logHandler.addModule(&serialModule);
+  Log.info("Initializing 'DevilRemote'");
+  Log.info( String("ESP ID: ") + ESP.getChipId() );
+
+  // Init Submodules
   setup_fastled();
   setup_mosfetLED();
+  setup_RotaryEncoder();
+
+  setup_WiFi();
+  setup_MQTT();
+  setup_ArduinoOTA();
+  
+  setup_sensor_dht();
+  setup_dallas();
+  
+  setup_Maintanance();
+
+  Log.info("Setup done");
 }
 
 void loop() {
-  loop_mqtt();
+  static bool enteredLoop = false;
+  if ( !enteredLoop ) {
+    enteredLoop = true;
+    Log.info("Entering loop()");
+  }
+
+  loop_MQTT();
+  loop_ArduinoOTA();
+
   loop_sensor_dht();
 
   threadControl.run();
