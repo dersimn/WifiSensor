aba4ff523a38cb825e3a393ce86a3a4c9eede1a9
Simon Christmann <simon@christmann.email>
2017-09-15 19:17:00 +0200

    Copy over newer versions
    Need to find a solution (maybe git submodules) to share pseudo-libraries between projects

--- handy_functions.h
+++ handy_functions.h
@@ -20,3 +20,8 @@ float rescale(float input, float input_range, float output_range) {
   return input / input_range * output_range;
 }
 
+int limit(int input, int min, int max) {
+  if ( input > max ) return max;
+  if ( input < min ) return min;
+  return input;
+}
